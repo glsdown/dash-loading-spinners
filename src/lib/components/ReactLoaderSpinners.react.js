16c5ed95be16e0d3a5c0a846b483351ff85c8d17
glsdown <gemmalsdown@icloud.com>
2021-06-24 00:24:55 +0100

    Added react-loader-spinners

---
+++ src/lib/components/ReactLoaderSpinners.react.js
@@ -0,0 +1,35 @@
+import Audio from './ReactLoaderSpinners/Audio.react';
+import BallTriangle from './ReactLoaderSpinners/BallTriangle.react';
+import Bars from './ReactLoaderSpinners/Bars.react';
+import Circles from './ReactLoaderSpinners/Circles.react';
+import GridAlt from './ReactLoaderSpinners/GridAlt.react';
+import Hearts from './ReactLoaderSpinners/Hearts.react';
+import MutatingDots from './ReactLoaderSpinners/MutatingDots.react';
+import Oval from './ReactLoaderSpinners/Oval.react';
+import Plane from './ReactLoaderSpinners/Plane.react';
+import PuffAlt from './ReactLoaderSpinners/PuffAlt.react';
+import RevolvingDot from './ReactLoaderSpinners/RevolvingDot.react';
+import Rings from './ReactLoaderSpinners/Rings.react';
+import TailSpin from './ReactLoaderSpinners/TailSpin.react';
+import ThreeDots from './ReactLoaderSpinners/ThreeDots.react';
+import Triangle from './ReactLoaderSpinners/Triangle.react';
+import Watch from './ReactLoaderSpinners/Watch.react';
+
+export {
+    Audio,
+    BallTriangle,
+    Bars,
+    Circles,
+    GridAlt,
+    Hearts,
+    MutatingDots,
+    Oval,
+    Plane,
+    PuffAlt,
+    RevolvingDot,
+    Rings,
+    TailSpin,
+    ThreeDots,
+    Triangle,
+    Watch,
+};
