96457f6eaf763abd22ec55a6825d120acec91452
glsdown <gemmalsdown@icloud.com>
2021-06-23 22:16:36 +0100

    Renamed files

--- dash_loading_spinners/_imports_.py
+++ dash_loading_spinners/_imports_.py
@@ -1,7 +1,43 @@
-from .Loader import Loader
 from .Beat import Beat
+from .Bounce import Bounce
+from .Circle import Circle
+from .ClimbingBox import ClimbingBox
+from .Clip import Clip
+from .Clock import Clock
+from .Dot import Dot
+from .Fade import Fade
+from .Grid import Grid
+from .Hash import Hash
+from .Moon import Moon
+from .Pacman import Pacman
+from .Propagate import Propagate
+from .Puff import Puff
+from .Pulse import Pulse
+from .Ring import Ring
+from .Rise import Rise
+from .Rotate import Rotate
+from .Scale import Scale
+from .Sync import Sync
 
 __all__ = [
-    "Loader",
-    "Beat"
+    "Beat",
+    "Bounce",
+    "Circle",
+    "ClimbingBox",
+    "Clip",
+    "Clock",
+    "Dot",
+    "Fade",
+    "Grid",
+    "Hash",
+    "Moon",
+    "Pacman",
+    "Propagate",
+    "Puff",
+    "Pulse",
+    "Ring",
+    "Rise",
+    "Rotate",
+    "Scale",
+    "Sync"
 ]
--- src/DashLoadingSpinners.jl
+++ src/DashLoadingSpinners.jl
@@ -5,8 +5,26 @@ using Dash
 const resources_path = realpath(joinpath( @__DIR__, "..", "deps"))
 const version = "0.0.1"
 
-include("dls_loader.jl")
 include("dls_beat.jl")
+include("dls_bounce.jl")
+include("dls_circle.jl")
+include("dls_climbingbox.jl")
+include("dls_clip.jl")
+include("dls_clock.jl")
+include("dls_dot.jl")
+include("dls_fade.jl")
+include("dls_grid.jl")
+include("dls_hash.jl")
+include("dls_moon.jl")
+include("dls_pacman.jl")
+include("dls_propagate.jl")
+include("dls_puff.jl")
+include("dls_pulse.jl")
+include("dls_ring.jl")
+include("dls_rise.jl")
+include("dls_rotate.jl")
+include("dls_scale.jl")
+include("dls_sync.jl")
 
 function __init__()
     DashBase.register_package(
