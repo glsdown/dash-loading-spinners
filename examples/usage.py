4521a6969f9094d96905c1fa7920c8d93d76231f
glsdown <gemmalsdown@icloud.com>
2021-06-30 22:15:48 +0100

    Tidy root

--- examples/usage.py
+++ examples/usage.py
@@ -2,7 +2,6 @@ import time
 
 import dash
 import dash_bootstrap_components as dbc
-import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
